0259b0c316c4d2f96bd52d60cbf98b0bcc15b049
samuzaffar99 <56280132+samuzaffar99@users.noreply.github.com>
2020-04-19 12:47:49 +0500

    converted to class and using vector

--- ToaCode.cpp
+++ ToaCode.cpp
@@ -1,11 +1,12 @@
-#include <algorithm> //vv  nice Abdullah 
+#include <algorithm> //vv  nice Abdullah
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <list> 
-#include <iterator> 
-#include<cstring>
+#include <list>
+#include <iterator>
+#include <cstring>
 #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 int gnWords=0;
 bool S(string s);
@@ -18,18 +19,18 @@ bool Verb(string s);
 bool Noun(string s);
 bool PP(string s);
 bool Preposition(string s);
+bool ProperNoun(string s);
 void Replace(string s1);
 void Showlist(list <string> g);
 //list
 list <string> plist;
 list<string>::iterator itr;
 //end list
-void Showlist(list <string> g)
-{ 
-    list <string> :: iterator it; 
-    for(it = g.begin(); it != g.end(); ++it) 
-        cout << *it<< " "; 
-    cout <<endl; 
+void Showlist(list <string> g){
+    list <string> :: iterator it;
+    for(it = g.begin(); it != g.end(); ++it)
+        cout << *it<< " ";
+    cout <<endl;
 }
 void Replace(string s){
 	itr=plist.erase(itr);
@@ -72,143 +73,272 @@ bool Finder(string s,string FN,string str){
     while(!F.eof()){
         getline(F,s2);
         if(s2==s){
-        	Replace(str);
-			Showlist(plist);	
-            Replace(s2); //debug
-            Showlist(plist);
+//        	Replace(str);
+//			Showlist(plist);
+//            Replace(s2); //debug
+//            Showlist(plist);
             return true;
         }
     }
     return false;
 }
-bool S(string s){
-	plist.push_back("NP");
-	plist.push_back("VP");
-	itr=plist.begin();
-	Showlist(plist);
-    if(NP(s)){
-    	itr++;
-        if(VP(WordCut(s))){
-             //debug
-            return true;
-        }
-        return true;
-    }
-   // cout<<"Failed S"<<endl; //debug
-    return false;
-}
 
-bool NP(string s){
-    if(Pronoun(s)){
-    	
-         //debug
-        return true;
-    }
-//    cout<<"Returned false\n";
-//    cout<<*itr<<endl;
-//    if(ProperNoun(s)){
-//        return true;
-//    }
-    if(Det(s)){
-    	//cout<<"here\n"; //debug
-    	plist.push_back("Nominal");
-    		Showlist(plist);
-
-		itr++;
-        if(Nominal(WordCut(s))){
-            //cout<<"Det Nominal"<<endl; //debug
-            return true;
+class Parser{
+string input; //input string
+int nWords; //Number of words
+vector<string> V;
+bool DebugOut;
+public:
+    Parser(){
+        cout<<"Enter the sentence to parse\n";
+        getline(cin,input); //get string from user
+        nWords=NumOfWords(input);
+        DebugOut=0;
+        if(DebugOut){
+            cout<<"Number of Words in string: "<<nWords<<endl; //debug
         }
     }
-    //cout<<"Failed NP"<<endl; //debug
-    return false;
-}
 
-bool Nominal(string s){
-    if(Noun(s)){
-        if(NumOfWords(s)>1){
-            if(Nominal(WordCut(s))){
-               // cout<<"Noun Nominal"<<endl; //debug
+    void Parse(){
+        if(input!=""){
+            S(input);
+            if(DebugOut){
+                cout<<"S"<<endl; //debug
+            }
+        }
+    }
+
+    void Print(){
+        if(V.empty()){
+            cout<<"Incorrect Structure"<<endl;
+            return;
+        }
+        string t;
+        int n=0;
+        V.push_back("S");
+        cout<<"Vector Contains: "<<endl;
+        for(int i=0;i<V.size();i++){
+            cout<<V.at(i)<<endl;
+        }
+        cout<<endl;
+        while(!V.empty()){
+            t=V.back();
+            V.pop_back();
+            if(t=="S" || t=="NP" || t=="VP" || t=="PP" || t=="Nominal" || t=="NP VP"){
+                for(int i=0,nt=0;nt!=n;i++){
+                    if(input[i]==' '){
+                        nt++;
+                    }
+                    cout<<input[i];
+                }
+                cout<<t<<endl;
+            }
+            if(t=="Preposition" || t=="Noun" || t=="Verb" || t=="Det" || t=="Pronoun"){
+                for(int i=0,nt=0;nt!=n;i++){
+                    if(input[i]==' '){
+                        nt++;
+                    }
+                    cout<<input[i];
+                }
+                cout<<t<<endl;
+                n++;
+                for(int i=0,nt=0;nt!=n;i++){
+                    if(input[i]==' ' || input[i]==0){
+                        nt++;
+                    }
+                    cout<<input[i];
+                }
+                cout<<endl;
+            }
+            if(t=="Det Nominal" || t=="Noun Nominal" || t=="ProperNoun" || t=="Preposition NP" || t=="Verb NP" || t=="Verb NP PP" || t=="Verb PP"){
+                for(int i=0,nt=0;nt!=n;i++){
+                    if(input[i]==' '){
+                        nt++;
+                    }
+                    cout<<input[i];
+                }
+                cout<<t<<endl;
+                n++;
+                for(int i=0,nt=0;nt!=n;i++){
+                    if(input[i]==' ' || input[i]==0){
+                        nt++;
+                    }
+                    cout<<input[i];
+                }
+                t=WordCut(t);
+                cout<<t<<endl;
+            }
+        }
+    }
+
+    bool S(string s){
+//        plist.push_back("NP");
+//        plist.push_back("VP");
+//        itr=plist.begin();
+//        Showlist(plist);
+        if(NP(s)){
+//            itr++;
+            if(VP(WordCut(s))){
+                V.push_back("NP VP");
+                if(DebugOut){
+                    cout<<"NP VP"<<endl; //debug
+                }
                 return true;
             }
         }
-        //cout<<"Noun"<<endl; //debug
-        return true;
+        if(DebugOut){
+            cout<<"Failed S"<<endl; //debug
+        }
+        return false;
     }
-    //cout<<"Failed Nominal"<<endl; //debug
-    return false;
-}
 
-bool VP(string s){
-//	itr++;
-//	Replace("Verb");
-//	Showlist(plist);
-    if(Verb(s)){
-    		plist.push_back("NP");
-    			Showlist(plist);
-
-			itr++;
-        if(NP(WordCut(s))){
-        	
-//            if(PP(WordCut(WordCut(s)))){
-//                //cout<<"Verb NP PP"<<endl; //debug
-//                return true;
-//            }
-            //cout<<"Verb NP"<<endl; //debug
-            return true;
-//        }
-        //cout<<"Verb"<<endl;
-        return true;
-//        if(PP(WordCut(s))){
-//            //cout<<"Verb NP"<<endl; //debug
+    bool NP(string s){
+        if(Pronoun(s)){
+            V.push_back("Pronoun");
+            if(DebugOut){
+                cout<<"Pronoun"<<endl; //debug
+            }
             return true;
         }
-        
-    }
-    //cout<<"Failed VP"<<endl; //debug
-    return false;
-}
-
-bool PP(string s){
-    if(Preposition(s)){
-        if(NP(WordCut(s))){
-            //cout<<"Preposition NP"<<endl; //debug
+//        cout<<*itr<<endl;
+        if(ProperNoun(s)){
+            V.push_back("ProperNoun");
+            if(DebugOut){
+                cout<<"ProperNoun"<<endl; //debug
+            }
             return true;
         }
+        if(Det(s)){
+//            cout<<"here\n"; //debug
+//            plist.push_back("Nominal");
+//            Showlist(plist);
+//            itr++;
+            if(Nominal(WordCut(s))){
+                V.push_back("Det Nominal");
+                if(DebugOut){
+                    cout<<"Det Nominal"<<endl; //debug
+                }
+                return true;
+            }
+        }
+        if(DebugOut){
+            cout<<"Failed NP"<<endl; //debug
+        }
+        return false;
     }
-    //cout<<"Failed PP"<<endl; //debug
-    return false;
-}
-bool Pronoun(string s){
-    return Finder(s,"pronouns.txt","Pronoun");
-}
 
-bool Preposition(string s){
-    return Finder(s,"prepositions.txt","Preposition");
-}
+    bool Nominal(string s){
+        if(Noun(s)){
+            if(NumOfWords(s)>1){
+                if(Nominal(WordCut(s))){
+                    V.push_back("Noun Nominal");
+                    if(DebugOut){
+                        cout<<"Noun Nominal"<<endl; //debug
+                    }
+                    return true;
+                }
+            }
+            V.push_back("Noun");
+            if(DebugOut){
+                cout<<"Noun"<<endl; //debug
+            }
+            return true;
+        }
+        if(DebugOut){
+            cout<<"Failed Nominal"<<endl; //debug
+        }
+        return false;
+    }
 
-bool Noun(string s){
-    return Finder(s,"nouns.txt","Noun");
-}
+    bool VP(string s){
+//    	itr++;
+//    	Replace("Verb");
+//    	Showlist(plist);
+        if(Verb(s)){
+//                plist.push_back("NP");
+//                Showlist(plist);
+//                itr++;
+            if(NP(WordCut(s))){
+                if(PP(WordCut(WordCut(s)))){
+                    V.push_back("Verb NP PP");
+                    if(DebugOut){
+                        cout<<"Verb NP PP"<<endl; //debug
+                    }
+                    return true;
+                }
+                V.push_back("Verb NP");
+                if(DebugOut){
+                    cout<<"Verb NP"<<endl; //debug
+                }
+                return true;
+            }
+            V.push_back("Verb");
+            if(DebugOut){
+                cout<<"Verb"<<endl;
+            }
+            return true;
+            if(PP(WordCut(s))){
+                V.push_back("Verb PP");
+                if(DebugOut){
+                    cout<<"Verb PP"<<endl; //debug
+                }
+                return true;
+            }
 
-bool Verb(string s){
-    return Finder(s,"verbs.txt","Verb");
-}
-bool Det(string s){
-    return Finder(s,"determiners.txt","Det");
-}
+        }
+        if(DebugOut){
+            cout<<"Failed VP"<<endl; //debug
+        }
+        return false;
+    }
+
+    bool PP(string s){
+        if(Preposition(s)){
+            if(NP(WordCut(s))){
+                V.push_back("Preposition NP");
+                if(DebugOut){
+                    cout<<"Preposition NP"<<endl; //debug
+                }
+                return true;
+            }
+        }
+        if(DebugOut){
+            cout<<"Failed PP"<<endl; //debug
+        }
+        return false;
+    }
+
+    bool Pronoun(string s){
+        return Finder(s,"pronouns.txt","Pronoun");
+    }
+
+    bool Preposition(string s){
+        return Finder(s,"prepositions.txt","Preposition");
+    }
+
+    bool Noun(string s){
+        return Finder(s,"nouns.txt","Noun");
+    }
+
+    bool Verb(string s){
+        return Finder(s,"verbs.txt","Verb");
+    }
+
+    bool Det(string s){
+        return Finder(s,"determiners.txt","Det");
+    }
+
+    bool ProperNoun(string s){
+        return false;
+    }
+};
 
 int main(){
-	string input; //input string
-	cout<<"Enter the sentence to parse\n";
-	getline(cin,input); //get string from user
-	int nWords=NumOfWords(input);
-	gnWords=nWords;
-	cout<<"Number of Words in string: "<<nWords<<endl; //debug
-	WordCut(input);
-	cout<<"S\n";
-	S(input);
-	
-
+    Parser A;
+    A.Parse();
+    A.Print();
   return 0;
 }
+
+
+
